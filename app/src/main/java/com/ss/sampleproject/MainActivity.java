f036a7e690b6aa9438d068fbf65a5f65b9e6a36f
shankarpilli <shankarraopilli@gmail.com>
2017-12-17 11:01:11 +0530

    Added some files

--- app/src/main/java/com/ss/sampleproject/MainActivity.java
+++ app/src/main/java/com/ss/sampleproject/MainActivity.java
@@ -1,13 +1,54 @@
 package com.ss.sampleproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends Activity {
+
+    public static String mName = "sample";
+
+    /*Button btn_login;
+    TextView tv_login;*/
+    @BindView(R.id.btn_login)
+    Button btn_login;
+    @BindView(R.id.tv_login)
+    TextView tv_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+       /* btn_login = (Button) findViewById(R.id.btn_login);
+        tv_login = (TextView) findViewById(R.id.tv_login);
+*/
+      /*  tv_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Toast.makeText(MainActivity.this, "Click", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("mobile_nubmer", "9014332627");
+                intent.putExtra("password", "545421");
+                startActivity(intent);
+            }
+        });*/
     }
+
+    @OnClick(R.id.tv_login)
+    void navigateSecondScreen(){
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        intent.putExtra("mobile_nubmer", "9014332627");
+        intent.putExtra("password", "545421");
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/ss/sampleproject/SecondActivity.java
+++ app/src/main/java/com/ss/sampleproject/SecondActivity.java
@@ -1,13 +1,24 @@
 package com.ss.sampleproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SecondActivity extends Activity {
+
+    private Intent intent;
+    private String mobile_nubmer;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        intent = getIntent();
+        mobile_nubmer = intent.getStringExtra("mobile_nubmer");
+        password = intent.getStringExtra("password");
+        Toast.makeText(SecondActivity.this, mobile_nubmer, Toast.LENGTH_SHORT).show();
+        Toast.makeText(SecondActivity.this, password, Toast.LENGTH_SHORT).show();
     }
 }
