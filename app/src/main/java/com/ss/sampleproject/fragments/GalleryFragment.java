dd90d25bf2e093b251c5c37c532c2c4728de386c
shankarpilli <shankarraopilli@gmail.com>
2017-12-25 10:41:43 +0530

    date and time pickers

--- app/src/main/java/com/ss/sampleproject/fragments/GalleryFragment.java
+++ app/src/main/java/com/ss/sampleproject/fragments/GalleryFragment.java
@@ -2,10 +2,13 @@ package com.ss.sampleproject.fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.ss.sampleproject.R;
 
@@ -15,16 +18,28 @@ import com.ss.sampleproject.R;
 public class GalleryFragment extends Fragment {
     public static String TAG = GalleryFragment.class.getName();
 
-    public GalleryFragment() {
-        // Required empty public constructor
-    }
+    private Bundle bundle;
+    private String mNumber = "";
 
+    private TextView tv_gallery;
+    private View view;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bundle = getArguments();
+        mNumber = bundle.getString("number");
+        Log.d(TAG, mNumber);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gallery, container, false);
+        view = inflater.inflate(R.layout.fragment_gallery, container, false);
+        tv_gallery = view.findViewById(R.id.tv_gallery);
+        tv_gallery.setText(mNumber);
+        return view;
     }
 
 }
--- app/src/main/java/com/ss/sampleproject/fragments/HomeFragment.java
+++ app/src/main/java/com/ss/sampleproject/fragments/HomeFragment.java
@@ -1,13 +1,27 @@
 package com.ss.sampleproject.fragments;
 
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.ss.sampleproject.R;
+
+import java.util.Calendar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,16 +29,117 @@ import com.ss.sampleproject.R;
 public class HomeFragment extends Fragment {
     public static String TAG = HomeFragment.class.getName();
 
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
+    private View view;
+    private EditText edt_number;
+    private static EditText edt_date;
+    private Button btn_home;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        initUI();
+        return view;
+    }
+
+    /**
+     * Initilization
+     */
+    private void initUI() {
+        edt_number = (EditText) view.findViewById(R.id.edt_number);
+        edt_date = (EditText) view.findViewById(R.id.edt_date);
+        btn_home = (Button) view.findViewById(R.id.btn_home);
+        btn_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isValidFields()) {
+                    Bundle bundle = new Bundle();
+                    bundle.putString("number", edt_number.getText().toString());
+                    navigate(new GalleryFragment(), GalleryFragment.TAG, bundle);
+                }
+            }
+        });
+
+        edt_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              /*  DatePickerFragment datePickerFragment = new DatePickerFragment();
+                datePickerFragment.show(getActivity().getSupportFragmentManager(), "datePicker");*/
+                TimePickerFragment timePickerFragment = new TimePickerFragment();
+                timePickerFragment.show(getActivity().getSupportFragmentManager(), "timePicker");
+            }
+        });
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            edt_date.setText(year + "-" + (month + 1) + "-" + day);
+        }
+    }
+
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time chosen by the user
+
+            edt_date.setText(hourOfDay + "-" + minute);
+        }
+    }
+
+    private boolean isValidFields() {
+        boolean isValid = true;
+        if (edt_number.getText().toString().length() < 10) {
+            Toast.makeText(getActivity(), "Number must be 10 digits", Toast.LENGTH_SHORT).show();
+            edt_number.requestFocus();
+            isValid = false;
+        } else if (edt_date.getText().toString().length() < 1) {
+            Toast.makeText(getActivity(), "Enter date", Toast.LENGTH_SHORT).show();
+            edt_date.requestFocus();
+            isValid = false;
+        }
+        return isValid;
+    }
+
+    private void navigate(Fragment fragment, String tag, Bundle bundle) {
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager
+                .beginTransaction();
+        if (bundle != null) {
+            fragment.setArguments(bundle);
+        }
+        fragmentTransaction.replace(R.id.content_frame, fragment, tag);
+        fragmentTransaction.addToBackStack(tag);
+        fragmentTransaction.commit();
     }
 
 }
